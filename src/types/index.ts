ad883cb98d8a75933d27a905d781ea72d1a30dbc
nicsylvia <nicsylvia15f@gmail.com>
2023-04-03 17:12:50 -0700

    Relearning api consumption

---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export type IUSER = {
+  name: string;
+  email: string;
+  password: string;
+};
--- src/utils/api/apiCalls.ts
+++ src/utils/api/apiCalls.ts
@@ -1,6 +1,20 @@
+import { IUSER } from "../../types";
 import axios from "./api";
 
 export const FetchDictionary = async () => {
+  await axios
+    .get("/define")
+    .then((res) => res.data)
+    .catch((err) => {
+      return err;
+    });
+};
+
+export const UploadDictionary = async (
+  data: IUSER,
+  id: string,
+  walletid: string
+) => {
   await axios
     .get("/define")
     .then((res) => res.data)
